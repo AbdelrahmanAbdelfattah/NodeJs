a9fce0c3a687f3a6079fe69061ce0331555cdb34
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-08-21 19:18:30 +0300

    Auth skeleton + passport packages + dotenv

--- Security/security-example-master/server.js
+++ Security/security-example-master/server.js
@@ -4,19 +4,38 @@ const https = require("https");
 const express = require("express");
 //helmet is a group of middlewares that enhance the security
 const helmet = require("helmet");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
+
+require("dotenv").config();
 
 const PORT = 3000;
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
 
 const app = express();
 
 app.use(helmet());
 
-app.get("/secret", (req, res) => {
-  return res.send("Your personal secret value is 42!");
-});
+function checkLoggedin(req, res, next) {
+  const isLoggedIn = true; //TODO
+  if (isLoggedIn) {
+    return res.status(401).json({ error: "You must log in" });
+  }
+  next();
+}
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("/auth/google", (req, res) => {});
+
+app.get("/auth/google/callback", (req, res) => {});
+
+app.get("/auth/logout", (req, res) => {});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
+  return res.send("Your personal secret value is 42!");
 });
 
 https
